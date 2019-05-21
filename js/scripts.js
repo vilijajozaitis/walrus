b6f518191ddde6353a568fb860d8c50f779707f6
katya lobanova and Vilija Jozaitis <katya@>
2019-05-20 19:35:49 -0700

    add new project repository for walrus page, css styleshees, js file

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".clickable-show").click(function() {
+    $("#walrus").fadeIn();
+    });
+  $(".clickable-hide").click(function() {
+    $("#walrus").fadeOut();
+  });
+});
+
+  // jQuery("h1").click(function() {
+  //   alert("This is a header.");
+  // });
+  //
+  // jQuery("p").click(function() {
+  //   alert("This is a paragraph.");
+  // });
+  //
+  // jQuery("img").click(function() {
+  //   alert("This is an image.");
+  // });

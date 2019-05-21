1f88c748e013bb703a4d655fb194ba75035d5d87
katya lobanova and Vilija Jozaitis <katya@>
2019-05-20 19:45:00 -0700

    add colors.html file, update css stylesheet and js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
-  $(".clickable-show").click(function() {
-    $("#walrus").fadeIn();
-    });
-  $(".clickable-hide").click(function() {
-    $("#walrus").fadeOut();
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
+
+
+// $(document).ready(function() {
+//   $(".clickable-show").click(function() {
+//     $("#walrus").fadeIn();
+//     });
+//   $(".clickable-hide").click(function() {
+//     $("#walrus").fadeOut();
+//   });
+// });
 
   // jQuery("h1").click(function() {
   //   alert("This is a header.");
